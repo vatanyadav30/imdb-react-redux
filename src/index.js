717d64689d60d70d7ee1632006c91e95bc362831
Vatan Yadav <vatanyadav@Vatans-MacBook-Air.local>
2024-03-29 13:54:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
-  <Provider store={store}>
-    <MovieApp/>
-  </Provider>  
-  //</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <MovieApp/>
+    </Provider>  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
